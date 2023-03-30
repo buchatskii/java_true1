4513af69e66d6a7709b850d4a7e9652cf0e41d26
buchatskii <103274724+buchatskii@users.noreply.github.com>
2023-03-31 00:54:02 +0300

    Задание 2. Пример 1

--- src2/Main.java
+++ src2/Main.java
@@ -1,1 +1,23 @@
-1
+import java.util.Arrays;
+
+public class Main4 {
+    public static void main(String[] args) {
+        String enterString = "select * from students where ";
+        String inputStr = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}";
+
+        StringBuilder outStr = new StringBuilder();
+        String newInputStr = inputStr
+                .replace("\"", "")
+                .replace("{", "")
+                .replace("}", "")
+                .replace(" ", "");
+        String[] newInputStrArr = newInputStr.split(",");
+        System.out.println(Arrays.toString(newInputStrArr));
+        for (String x : newInputStrArr) {
+            if (!x.contains("null")) {
+                outStr.append(enterString + x + "\n");
+            }
+        }
+        System.out.println(outStr);
+    }
+}
