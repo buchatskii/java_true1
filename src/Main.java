a33573992405912a018cfd1be92f617856fbc28a
buchatskii <103274724+buchatskii@users.noreply.github.com>
2023-03-31 00:44:35 +0300

    Задание 1. Пример 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int number = sc.nextInt();
+        System.out.println("Сумма входящих чисел " + triangleNum(number));
+        System.out.printf("Произведение входящих чисел = %d", factNum(number));
+        sc.close();
+    }
+
+
+    public static int triangleNum(int number) {
+        int triangleNumber = (number * (number + 1)) / 2;
+        return triangleNumber;
+    }
+
+    public static int factNum(int number) {
+        if (number == 0) return 1;
+        return number*factNum(number-1);
+
     }
 }
